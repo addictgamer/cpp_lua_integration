72c4b4ab42e25bc0e59f79dc84cb7d7f45672adf
Ciprian <ciliesj@gmail.com>
2018-01-27 21:50:43 -0800

    * Minimal LUA integration

--- source/src/luatest/main.cpp
+++ source/src/luatest/main.cpp
@@ -5,9 +5,19 @@
 #include <iostream>
 using std::cout;
 
+#include <luajit-2.0/lua.hpp>
+
 int main(int argc, char** argv)
 {
+	lua_State *lua = lua_open();
+
 	cout << "Potato.\n";
+
+	luaL_openlibs(lua);
+
+	luaL_dofile(lua, "foo.lua");
+
+	lua_close(lua);
 
 	return 0;
 }
